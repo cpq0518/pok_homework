44000c42a6db79e11d5fdb4cae4edac4597bd0ae
TeraniteAK <leizhanyao@gmail.com>
2020-12-15 16:45:03 +0800

    [add]test_rr FOLDER: rr algorithm

--- libpok/libc/misc/.depend.mk
+++
@@ -1,6 +0,0 @@
-__udivdi3.o: __udivdi3.c \
- /home/leizhanyao/develop/pok_homework/pok_homework/examples/semaphores/pr1/deployment.h \
- ../../..//libpok/include/core/dependencies.h
-__umoddi3.o: __umoddi3.c \
- /home/leizhanyao/develop/pok_homework/pok_homework/examples/semaphores/pr1/deployment.h \
- ../../..//libpok/include/core/dependencies.h
